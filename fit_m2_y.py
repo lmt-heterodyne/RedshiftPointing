d426b6ccdacc20eaa8b76f5b5eae60c49278beaf
Kamal Souccar <souccar@umass.edu>
2014-10-28 17:10:39 +0000

    added

---
+++ fit_m2_y.py
@@ -0,0 +1,35 @@
+"""Module with the fit_m2 script which finds optimum position of M2 from pointing maps.
+
+Uses:    sys, RSRMapController, RSRHandleArgs, RSRMapFit, RSRM2Fit, RSRM2FitViewer
+Author:  FPS
+Date:    May 5, 2014
+Changes:
+"""
+import sys
+
+from RSRController import RSRMapController,RSRHandleArgs
+from RSRFit import RSRMapFit
+from RSRFit import RSRM2Fit
+from RSRViewer import RSRM2FitViewer
+
+def main(argv):
+    a = RSRHandleArgs()
+    C = RSRMapController()
+    V = RSRM2FitViewer()
+
+    check = a.parse_args(argv,'fit_m2',1)
+
+    if check == 0:
+        F = C.reduce_maps(a)
+
+        M = RSRM2Fit(F,1)
+
+        M.find_focus()
+        V.print_results(M)
+        V.print_summary_fit(M)
+
+        if a.show_it:
+            V.init()
+            V.plot_fits(M,figno=1)
+
+main(sys.argv[1:])
---
+++ look_mapscan.py
@@ -0,0 +1,53 @@
+
+import sys, getopt
+from dreampy.redshift.netcdf import RedshiftNetCDFFile
+import matplotlib.pyplot as pl
+
+def main(argv):
+    # defaults
+    data = '2013-04-21'
+    scan = '6991'
+    chassis = '2'
+    band = 0
+
+    # get options
+    try:
+        opts, arg = getopt.getopt(argv,"hc:d:s:b:",["chassis=","date=","scan=","band="])
+    except getopt.GetoptError:
+        print 'bad argument: usage: look_map.py'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'usage: look_map.py'
+            print '-c chassis'
+            print '-d date in yyyy-mm-dd format'
+            print '-s OBSNUM'
+            print '-b band (or board) to view'
+            sys.exit()
+        elif opt in ("-c","--chassis"):
+            chassis = arg
+        elif opt in ("-d","--date"):
+            date = arg
+        elif opt in ("-s","--scan"):
+            scan = arg
+        elif opt in ("-b","--band"):
+            band = eval(arg)
+    
+
+    filename = make_filename(date,scan,chassis)
+    nc = RedshiftNetCDFFile(filename)
+    pl.ion()
+    pl.clf()
+    pl.figure(1)
+    pl.plot(nc.hdu.data.AccAverage[:,band])
+    print 'Gains= ',nc.hdu.header.Gain
+    pl.show()
+
+
+def make_filename(date,scan,chassis):
+    # makes a filename from date and obnum
+    filename = '/data_lmt/RedshiftChassis%s/RedshiftChassis%s_%s_%06d_00_0001.nc' % (chassis, chassis, date, eval(scan))
+    return filename
+
+
+main(sys.argv[1:])
