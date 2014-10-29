7c714a29b29de30ffca74a2426e94fc7fec2dd77
rsr <rsr>
2014-10-29 16:44:47 +0000

    don't plot absolute pointing

--- RSRViewer.py
+++ RSRViewer.py
@@ -593,18 +593,6 @@ class RSRFitViewer(RSRViewer):
         textstr = textstr + 'El Map Offset:   %6.4f'%(F.mean_el_map_offset)
         props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
         pl.text(0, axis[2]*0.7, textstr, horizontalalignment='center', verticalalignment='top', bbox=props, color='red')
-        pl.subplot(1,2,2)
-        pl.plot(F.az_model_offset,F.el_model_offset,'o')
-        pl.xlabel('Az Offset (arcsec)')
-        pl.title('%s %d'%(F.date,F.obsnum))
-        pl.axis('equal')
-        pl.axis(axis)
-        pl.text(0,axis[3]*0.95,'Model %d Offset'%(F.modrev),horizontalalignment='center',verticalalignment='top')
-        pl.grid()
-        textstr =           'Az Model %d Offset:   %6.4f'%(F.modrev, F.mean_az_model_offset) + '\n' 
-        textstr = textstr + 'El Model %d Offset:   %6.4f'%(F.modrev, F.mean_el_model_offset)
-        props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-        pl.text(0, axis[2]*0.7, textstr, horizontalalignment='center', verticalalignment='top', bbox=props, color='red')
         pl.savefig('rsr_summary.png', bbox_inches='tight')
 
     def write_temperature_log_entry(self,F,tfile):
