4e3f0d25b98e18431fe1692386fd30606cd8ab5f
rsr <rsr>
2016-12-14 22:04:48 +0000

    run it on chassis 2

--- RSRRunTsys.py
+++ RSRRunTsys.py
@@ -9,7 +9,7 @@ from rsrFileSearch import rsrFileSearch
 class RSRRunTsys():
     def run(self, argv, obsNum):
 
-        filename = rsrFileSearch (obsNum, 0)
+        filename = rsrFileSearch (obsNum, 2)
         nc = RedshiftNetCDFFile(filename)
         nc.hdu.get_cal()    
         pl  = RedshiftPlotChart()
@@ -17,5 +17,5 @@ class RSRRunTsys():
         tsys = nanmean(nc.hdu.cal.Tsys.flatten())
         print 'Average Tsys = %6.2f K' % tsys
         pl.hlines(tsys,70,115)
-        pl.annotate("Average Tsys =%6.2fK"%tsys, [114,tsys-1], fontsize=13, fontweight='bold', stretch='250', horizontalalignment='right', verticalalignment='top')
+        pl.annotate("Average Tsys =%6.2fK on  Chassis %d"%(tsys,2), [114,tsys-1], fontsize=13, fontweight='bold', stretch='250', horizontalalignment='right', verticalalignment='top')
         pl.savefig('rsr_summary.png', bbox_inches='tight')
