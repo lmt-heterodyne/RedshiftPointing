ff6fce1f1f9818899f4c2f8619871bd4b7d71b31
rsr <rsr>
2016-12-15 20:24:49 +0000

    modified test files

--- runFocus.py
+++ runFocus.py
@@ -4,8 +4,8 @@
 import sys
 from RSRRunFocus import RSRRunFocus
 
-argv = ["-d", "2016-02-16", "-s", "56830:56833", "--show", "True"]
-#argv = ["-d", "2016-02-16", "-s", "10056830:10056833", "--show", "True"]
+#argv = ["-d", "2016-02-16", "-s", "56830:56833", "--show", "True"]
+argv = ["-d", "2016-02-16", "-s", "10056830:10056833", "--show", "True"]
 rsr = RSRRunFocus()
 F = rsr.run(argv)
 
