3ca0812ef47619c5bee09e445cc5e10e05ca9df9
rsr <rsr>
2016-12-16 19:13:38 +0000

    run from filelist

--- runPointing.py
+++ runPointing.py
@@ -10,9 +10,16 @@ argv = ["-d", "2014-11-01", "-s", "27064", "--show", "True"]
 argv = ["-d", "2015-03-01", "-s", "37603", "--show", "True", "--chassis", "[0]"]
 argv = ["-d", "2015-03-02", "-s", "37668", "--show", "True", "--throw" , "0", "--chassis", "[0]", "--board", "[0,1]", "--beam", "0"]
 argv = ["-d", "2015-03-18", "-s", "38496", "--show", "True"]
+argv = ["-d", "2016-02-16", "-s", "10056830", "--show", "True"]
 
+filelist = []
+filelist.append("./data_lmt/RedshiftChassis0_2016-02-16_10056830_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis1_2016-02-16_10056830_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis2_2016-02-16_10056830_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis3_2016-02-16_10056830_01_0001.nc");
 rsr = RSRRunPointing()
-F = rsr.run(argv)
+F = rsr.run(argv, filelist)
+#F = rsr.run(argv)
 print ('Average Pointing:      %5.1f %5.1f    %5.1f %5.1f            %5.1f %5.1f' % 
        (F.mean_az_map_offset,
         F.mean_el_map_offset,
