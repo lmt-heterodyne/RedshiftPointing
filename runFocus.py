72548f1351f6dde22b826d28729833679e1f018f
rsr <rsr>
2016-12-16 18:06:44 +0000

    run focus from filelist

--- runFocus.py
+++ runFocus.py
@@ -4,9 +4,28 @@
 import sys
 from RSRRunFocus import RSRRunFocus
 
-#argv = ["-d", "2016-02-16", "-s", "56830:56833", "--show", "True"]
-argv = ["-d", "2016-02-16", "-s", "10056830:10056833", "--show", "True"]
+argv = ["-d", "2016-02-16", "-s", "56830:56833", "--show", "True"]
+
+filelist = []
+filelist.append("./data_lmt/RedshiftChassis0_2016-02-16_10056830_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis0_2016-02-16_10056831_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis0_2016-02-16_10056832_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis0_2016-02-16_10056833_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis1_2016-02-16_10056830_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis1_2016-02-16_10056831_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis1_2016-02-16_10056832_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis1_2016-02-16_10056833_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis2_2016-02-16_10056830_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis2_2016-02-16_10056831_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis2_2016-02-16_10056832_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis2_2016-02-16_10056833_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis3_2016-02-16_10056830_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis3_2016-02-16_10056831_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis3_2016-02-16_10056832_01_0001.nc");
+filelist.append("./data_lmt/RedshiftChassis3_2016-02-16_10056833_01_0001.nc");
+
 rsr = RSRRunFocus()
-F = rsr.run(argv)
+#F = rsr.run(argv)
+F = rsr.run(argv, filelist, 4)
 
 raw_input("press any key to exit: ")
