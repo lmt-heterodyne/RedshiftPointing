7f3d5b40dff262238cbce51f8a7bf092f8a9bf48
rsr <rsr>
2017-01-30 19:10:42 +0000

    added Vlbi1mm

--- runTsys.py
+++ runTsys.py
@@ -3,9 +3,14 @@
 
 import sys
 from RSRRunTsys import RSRRunTsys
+from Vlbi1mmRunTsys import Vlbi1mmRunTsys
 
 ### CHANGE THESE ObsNum for every science spectrum ###
-obsNum = 27869
+#obsNum = 27869
+obsNum = 65223
 
-rsr = RSRRunTsys()
-F = rsr.run(sys.argv,obsNum)
+#rsr = RSRRunTsys()
+#F = rsr.run(sys.argv,obsNum)
+
+vlbi1mm = Vlbi1mmRunTsys()
+F = vlbi1mm.run(sys.argv,obsNum)
