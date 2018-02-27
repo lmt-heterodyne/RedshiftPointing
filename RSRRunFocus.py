09c0d1f6e5191db592cd3c47a687a2b47753be83
rsr <rsr>
2018-02-27 13:30:55 +0000

    combine summary and focus_fits plots

--- RSRRunFocus.py
+++ RSRRunFocus.py
@@ -1,4 +1,5 @@
 import sys
+from PIL import Image
 from RSRFit import RSRM2Fit
 from RSRViewer import RSRM2FitViewer
 from RSRController import RSRMapController,RSRHandleArgs
@@ -33,7 +34,30 @@ class RSRRunFocus():
                 v.plot_fits(m,figno=1)
                 v.plot_focus_model_fit(m,figno=2,obsNumArg=obsNumArg)
 
+            images = map(Image.open, ['rsr_summary.png', 'rsr_focus_fits.png'])
+            widths, heights = zip(*(i.size for i in images))
+
+            total_width = sum(widths)
+            max_height = max(heights)
+
+            new_im = Image.new('RGB', (total_width, max_height))
+
+            x_offset = 0
+            for im in images:
+                new_im.paste(im.resize((im.size[0], max_height), Image.ANTIALIAS), (x_offset,0))
+                x_offset += im.size[0]
+
+            desired_size = images[0].size[0]
+            old_size = new_im.size
+            ratio = float(desired_size)/max(old_size)
+            new_size = tuple([int(x*ratio) for x in old_size])
+            new_im = new_im.resize(new_size, Image.ANTIALIAS)
+            new_im2 = Image.new("RGB", (desired_size, desired_size))
+            new_im2.paste(new_im, ((desired_size-new_size[0])//2,
+                                   (desired_size-new_size[1])//2))
+
+
+            new_im2.save('rsr_summary.png')
+
             return m
-
-
 
