078490df1ddc31bbf28c836c34aa0b67fa652d9c
rsr <rsr>
2018-03-12 11:35:55 +0000

    trying other obsnums

--- runFocus.py
+++ runFocus.py
@@ -6,7 +6,6 @@ from RSRRunFocus import RSRRunFocus
 
 argv = ["-d", "2018-02-27", "-s", "73014:73020", "--chassis", "[1,2,3]", "--show", "True"]
 
-
 filelist = [
 "./data_lmt/RedshiftChassis1/RedshiftChassis1_2018-02-27_073014_01_0000.nc", 
 "./data_lmt/RedshiftChassis1/RedshiftChassis1_2018-02-27_073015_01_0000.nc", 
@@ -30,6 +29,33 @@ filelist = [
 "./data_lmt/RedshiftChassis3/RedshiftChassis3_2018-02-27_073019_01_0000.nc", 
 "./data_lmt/RedshiftChassis3/RedshiftChassis3_2018-02-27_073020_01_0000.nc",
     ]
+
+
+argv = ["-d", "2018-02-27", "-s", "73098:73103", "--chassis", "[1,2,3]", "--show", "True"]
+
+filelist = [
+    "./data_lmt/RedshiftChassis1/RedshiftChassis1_2018-02-27_073098_01_0000.nc", 
+    "./data_lmt/RedshiftChassis1/RedshiftChassis1_2018-02-27_073099_01_0000.nc", 
+    "./data_lmt/RedshiftChassis1/RedshiftChassis1_2018-02-27_073100_01_0000.nc",
+    "./data_lmt/RedshiftChassis1/RedshiftChassis1_2018-02-27_073101_01_0000.nc",
+    "./data_lmt/RedshiftChassis1/RedshiftChassis1_2018-02-27_073102_01_0000.nc",
+    "./data_lmt/RedshiftChassis1/RedshiftChassis1_2018-02-27_073103_01_0000.nc",
+
+    "./data_lmt/RedshiftChassis2/RedshiftChassis2_2018-02-27_073098_01_0000.nc", 
+    "./data_lmt/RedshiftChassis2/RedshiftChassis2_2018-02-27_073099_01_0000.nc", 
+    "./data_lmt/RedshiftChassis2/RedshiftChassis2_2018-02-27_073100_01_0000.nc",
+    "./data_lmt/RedshiftChassis2/RedshiftChassis2_2018-02-27_073101_01_0000.nc",
+    "./data_lmt/RedshiftChassis2/RedshiftChassis2_2018-02-27_073102_01_0000.nc",
+    "./data_lmt/RedshiftChassis2/RedshiftChassis2_2018-02-27_073103_01_0000.nc",
+
+    "./data_lmt/RedshiftChassis3/RedshiftChassis3_2018-02-27_073098_01_0000.nc", 
+    "./data_lmt/RedshiftChassis3/RedshiftChassis3_2018-02-27_073099_01_0000.nc", 
+    "./data_lmt/RedshiftChassis3/RedshiftChassis3_2018-02-27_073100_01_0000.nc",
+    "./data_lmt/RedshiftChassis3/RedshiftChassis3_2018-02-27_073101_01_0000.nc",
+    "./data_lmt/RedshiftChassis3/RedshiftChassis3_2018-02-27_073102_01_0000.nc",
+    "./data_lmt/RedshiftChassis3/RedshiftChassis3_2018-02-27_073103_01_0000.nc",
+    ]
+
 rsr = RSRRunFocus()
 #F = rsr.run(argv)
 F = rsr.run(argv, filelist, 7)
