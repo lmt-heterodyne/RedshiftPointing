d5050f7124420ae3971e39aa11e2f5e87a5cfd96
rsr <rsr>
2018-03-16 06:12:52 +0000

    added

---
+++ lmtTpmFileSearch.py
@@ -0,0 +1,33 @@
+from fnmatch import fnmatch
+from os  import listdir
+
+def lmtTpmFileSearch (obsnum, root='/data_lmt', full = True):
+	
+	baseDir = "%s/LmtTpm/"%root
+
+	listDir = listdir(baseDir)
+
+
+	for ifile in listDir:
+		if fnmatch(ifile,'LmtTpm*_%06d_*.nc' %obsnum):
+			if full:
+				ifile = baseDir+ifile
+			return ifile
+
+	return "" 
+
+def lmtTpmFileSearch (obsnum, root='/data_lmt', full = True):
+        all = []
+	
+	baseDir = "%s/LmtTpm/"%root
+
+	listDir = listdir(baseDir)
+
+
+	for ifile in listDir:
+		if fnmatch(ifile,'LmtTpm*_%06d_*.nc' %obsnum):
+			if full:
+				ifile = baseDir+ifile
+			all.append(ifile)
+
+	return all 
