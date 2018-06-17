723a6df513a898808effd648869bc1eff5b9764b
rsr <rsr>
2018-06-17 03:34:09 +0000

    added

---
+++ LmtTpmRunTsys.py
@@ -0,0 +1,101 @@
+import sys
+import os
+import glob
+import numpy
+from dreampy.lmtnetcdf import LMTNetCDFFile
+import matplotlib.pyplot as plt
+
+def most_recent_file(glob_pattern):
+    return max(glob.iglob(glob_pattern), key=os.path.getctime)
+
+def get_telnc_file(obsnum, basepath='/data_lmt/lmttpm',
+                   most_recent=True):
+    glob_pattern = os.path.join(basepath, "lmttpm_*_%06d_*.nc" % obsnum)
+    if most_recent:
+        return most_recent_file(glob_pattern)
+    fnames = glob.glob(glob_pattern)
+    if fnames:
+        return fnames[0]
+
+msip1mm_pixel_description = {0: 'P0_USB',
+                             1: 'P0_LSB',
+                             2: 'P1_LSB',
+                             3: 'P1_USB'}
+
+def get_Tsys(obsnum, msip1mm=True):
+    try:
+        telnc = LMTNetCDFFile(get_telnc_file(obsnum))
+    except:
+        print "Cannot find filename with obsnum %d" % obsnum
+        return []
+    if telnc.hdu.header.get('Dcs.ObsPgm') != 'Cal':
+        print "Not a CAL Scan"
+        return []
+    if not telnc.hdu.header.has_key('LmtTpm'):
+        print "Not an LmtTpm Scan"
+        return []
+    rx = telnc.hdu.header.get('Dcs.Receiver')
+    if rx != 'Msip1mm':
+        msip1mm = False
+    time = telnc.hdu.data.TelTime
+    idxh = numpy.where(telnc.hdu.data.BufPos == 3)
+    idxc = numpy.where(telnc.hdu.data.BufPos == 2)
+    numpixels = 1 #telnc.hdu.data.Signal.shape[1]
+    Tsys = []
+    for pixel in range(numpixels):
+        dic = {}
+        Ph = telnc.hdu.data.Signal[idxh, pixel].mean()
+        Pc = telnc.hdu.data.Signal[idxc, pixel].mean()
+        dic['Tsys'] = 280 * Pc/(Ph - Pc)
+        if msip1mm:
+            dic['desc'] = msip1mm_pixel_description.get(pixel)
+        else:
+            dic['desc'] = "Pixel %d" % (pixel)
+        Tsys.append(dic)
+    return time[-1],Tsys,rx
+
+    
+class LmtTpmRunTsys():
+    def run(self, argv, obsNum):
+
+        plotlabel = ""
+        time,tsys,rx = get_Tsys(obsNum)
+        if rx == 'Msip1mm':
+            with open('/var/www/vlbi1mm/vlbi1mm_tsys.html', 'w') as fp:
+                for d in tsys:
+                    desc = d['desc']
+                    val = d['Tsys']
+                    #print desc, val
+                    fp.write("%s %3.1f\n" %(desc, val))
+                    plotlabel = plotlabel + "%s %3.1f\n" %(desc, val)
+                fp.write("ObsNum %d\n" %(obsNum))
+                fp.write("Time %3.1f\n" %(time))
+                print plotlabel
+        for d in tsys:
+            desc = d['desc']
+            val = d['Tsys']
+            #print desc, val
+            plotlabel = plotlabel + "%s %3.1f\n" %(desc, val)
+        print plotlabel
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.text(0.1, 0.2, plotlabel, clip_on=True)
+	ax.set_title("ObsNum: %d"% obsNum)  
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        plt.savefig('rsr_summary.png', bbox_inches='tight')
+
+def main():
+    obsNum = 77926
+    try:
+        obsNum = int(sys.argv[1])
+    except Exception as e:
+        pass
+    tsys = LmtTpmRunTsys()
+    tsys.run(sys.argv, obsNum)
+
+if __name__ == '__main__':
+    main()
+    
+    
