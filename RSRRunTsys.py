e572f6fdb2f3b7d41f72dbd9c349672b58b9b556
rsr <rsr>
2020-02-20 19:59:09 +0000

    use chassis 1

--- RSRRunTsys.py
+++ RSRRunTsys.py
@@ -45,7 +45,8 @@ class RSRRunTsys():
 
     def run(self, argv, obsNum):
 
-        filename = genericFileSearch ('RedshiftChassis2', obsNum)
+        use_chassis = 1
+        filename = genericFileSearch ('RedshiftChassis%d'%use_chassis, obsNum)
         nc = RedshiftNetCDFFile(filename)
         nc.hdu.get_cal()
         #tsys = nanmean(nc.hdu.cal.Tsys.flatten())
@@ -65,11 +66,12 @@ class RSRRunTsys():
             self.plot_tsys(pl, FontProperties, nc)
 	    #pl.ylim(0.0,3*tsys)
         pl.hlines(tsys,70,115)
-        pl.annotate("Average Tsys =%6.2fK on  Chassis %d"%(tsys,2), [114,tsys-1], fontsize=13, fontweight='bold', stretch='250', horizontalalignment='right', verticalalignment='top')
+        pl.annotate("Average Tsys =%6.2fK on  Chassis %d"%(tsys,use_chassis), [114,tsys-1], fontsize=13, fontweight='bold', stretch='250', horizontalalignment='right', verticalalignment='top')
         pl.savefig('rsr_summary.png', bbox_inches='tight')
 
 if __name__ == '__main__':
-    obsNumList = [65970]
+    #obsNumList = [65970]
+    obsNumList = [91159]
     rsr = RSRRunTsys()
     M = rsr.run(sys.argv,obsNumList[-1])
         
