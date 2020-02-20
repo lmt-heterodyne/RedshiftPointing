a9647ca4c190e83387b9e70eaad9b495c7797c32
rsr <rsr>
2020-02-20 23:29:04 +0000

    handle all chassis

--- RSRRunTsys.py
+++ RSRRunTsys.py
@@ -1,11 +1,7 @@
 from dreampy.redshift.netcdf import RedshiftNetCDFFile
-try:
-    from dreampy.redshift.plots import RedshiftPlotChart
-    from dreampy.redshift.utils.correlate_lines import CrossCorrelation
-    dreampy_plot = True
-except:
-    dreampy_plot = False
 import numpy
+import matplotlib.pyplot as pl
+from matplotlib.font_manager import FontProperties
 try:
     from scipy.stats import nanmean
 except:
@@ -37,41 +33,63 @@ class RSRRunTsys():
         for board in boards:
             pl.plot(hdu.frequencies[board, :], hdu.cal.Tsys[board, :],
                       linestyle='steps-mid', label='%d.%d' % (int(hdu.header.ChassisNumber), board))
-        pl.xlabel("Frequency (GHz)")
         pl.legend(loc='best', prop=FontProperties(size="xx-small"))
-        pl.ylabel("Tsys (K)")
-        pl.title('%s: Source %s' % (hdu.header.obs_string(), hdu.header.SourceName))        
+        pl.set_ylabel("Tsys (K)")
 
+    def decode_chassis_string(self,chassis_arg):
+        """Decodes the argument provided with the -c flag."""
+        if chassis_arg[0] == 'a' or chassis_arg[0] == 'n':
+            self.chassis_list = [0,1,2,3]
+        elif chassis_arg[0] == '[':
+            self.chassis_list = eval(chassis_arg)
+        else:
+            p = chassis_arg.partition(':')
+            if p[1] == ':':
+                self.chassis_list = range(int(p[0]),int(p[2])+1)
+            else:
+                self.chassis_list = range(int(p[0]),int(p[0])+1)
+    
 
     def run(self, argv, obsNum):
 
-        use_chassis = 1
-        filename = genericFileSearch ('RedshiftChassis%d'%use_chassis, obsNum)
-        nc = RedshiftNetCDFFile(filename)
-        nc.hdu.get_cal()
-        #tsys = nanmean(nc.hdu.cal.Tsys.flatten())
-	tsys_data = nc.hdu.cal.Tsys.flatten()
-	tsys_data = tsys_data[numpy.where(numpy.isfinite(tsys_data))]
-	tsys = numpy.median (tsys_data)
-        print 'Average Tsys = %6.2f K' % tsys
-        if dreampy_plot:
-            print 'using dreampy_plot'
-            pl  = RedshiftPlotChart()
-            pl.plot_tsys(nc)
-	    pl.set_ylim(0.0,3*tsys)
-        else:
-            print 'using matplotlib_plot'
-            import matplotlib.pyplot as pl
-            from matplotlib.font_manager import FontProperties
-            self.plot_tsys(pl, FontProperties, nc)
-	    #pl.ylim(0.0,3*tsys)
-        pl.hlines(tsys,70,115)
-        pl.annotate("Average Tsys =%6.2fK on  Chassis %d"%(tsys,use_chassis), [114,tsys-1], fontsize=13, fontweight='bold', stretch='250', horizontalalignment='right', verticalalignment='top')
+        for i,arg in enumerate(argv):
+            if arg in ("-c","--chassis"):
+                self.decode_chassis_string(argv[i+1])
+        pl.clf()
+        for i,use_chassis in enumerate(self.chassis_list):
+            print(use_chassis)
+            ax = pl.subplot(len(self.chassis_list), 1, i+1)
+            filename = genericFileSearch ('RedshiftChassis%d'%use_chassis, obsNum)
+            try:
+                nc = RedshiftNetCDFFile(filename)
+            except:
+                ax.annotate("File not found for Chassis %d"%(use_chassis), [0.2,0.5], fontsize=13, fontweight='bold', stretch='250', horizontalalignment='left', verticalalignment='top')
+                continue
+            nc.hdu.get_cal()
+            #tsys = nanmean(nc.hdu.cal.Tsys.flatten())
+            tsys_data = nc.hdu.cal.Tsys.flatten()
+            tsys_data = tsys_data[numpy.where(numpy.isfinite(tsys_data))]
+            tsys = numpy.median (tsys_data)
+            print 'Average Tsys = %6.2f K' % tsys
+            self.plot_tsys(ax, FontProperties, nc)
+            #pl.ylim(0.0,3*tsys)
+            ax.hlines(tsys,70,115)
+            ax.annotate("Average Tsys =%6.2fK on  Chassis %d"%(tsys,use_chassis), [105,tsys-1], fontsize=13, fontweight='bold', stretch='250', horizontalalignment='right', verticalalignment='top')
+            if i == 0:
+                if isinstance(nc, RedshiftNetCDFFile):
+                    hdu = nc.hdu
+                elif isinstance(nc, RedshiftScan):
+                    hdu = nc
+                else:
+                    raise LMTRedshiftError("plot tsys", "Argument nc should be of type RedshiftNetCDFFile or RedshiftScan")
+                pl.title('%s: Source %s' % (hdu.header.obs_string(), hdu.header.SourceName))        
+
+        pl.xlabel("Frequency (GHz)")
         pl.savefig('rsr_summary.png', bbox_inches='tight')
 
 if __name__ == '__main__':
     #obsNumList = [65970]
-    obsNumList = [91159]
+    obsNumList = [91180]
     rsr = RSRRunTsys()
     M = rsr.run(sys.argv,obsNumList[-1])
         
