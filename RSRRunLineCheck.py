6eac413d12f0b835475e7d44be66d281f2e5dd3c
rsr <rsr>
2020-02-21 02:39:17 +0000

    get chassis_list from argv

--- RSRRunLineCheck.py
+++ RSRRunLineCheck.py
@@ -13,6 +13,20 @@ from genericFileSearch import genericFileSearch
 
 
 class RSRRunLineCheck():
+    def decode_chassis_string(self,chassis_arg):
+        """Decodes the argument provided with the -c flag."""
+        if chassis_arg[0] == 'a' or chassis_arg[0] == 'n':
+            self.chassis_list = [0,1,2,3]
+        elif chassis_arg[0] == '[':
+            self.chassis_list = eval(chassis_arg)
+        else:
+            p = chassis_arg.partition(':')
+            if p[1] == ':':
+                self.chassis_list = range(int(p[0]),int(p[2])+1)
+            else:
+                self.chassis_list = range(int(p[0]),int(p[0])+1)
+    
+
     def run(self, argv, obsList):
 
         hdulist = []
@@ -20,15 +34,21 @@ class RSRRunLineCheck():
         tint = 0.0
         real_tint = 0.0
 
+        actual_chassis_list = {}
+        self.chassis_list = [0, 1, 2, 3]
+
+        for i,arg in enumerate(argv):
+            if arg in ("-c","--chassis"):
+                self.decode_chassis_string(argv[i+1])
         for obsNum in obsList:
-            #for chassis in [0,1,2,3]:
-            for chassis in [1,2,3]:
+            obsnum_chassis_list = []
+            for chassis in self.chassis_list:
 
                 filename = genericFileSearch ('RedshiftChassis%d'%chassis, obsNum)
-	
                 if filename == "":
-                    raise Exception ('File not found for chassis %d obsNumber: %s ' % (chassis,obsNum))
-		
+                    continue
+
+                obsnum_chassis_list += [chassis]
 
                 nc = RedshiftNetCDFFile(filename)
                 nc.hdu.process_scan(corr_linear=False) 
@@ -39,6 +59,7 @@ class RSRRunLineCheck():
                 hdulist.append(nc.hdu)
                 nc.nc.sync()
                 nc.nc.close()
+            actual_chassis_list[obsNum] = obsnum_chassis_list
 
         
         hdu = hdulist[0]  # get the first observation
@@ -56,7 +77,7 @@ class RSRRunLineCheck():
             pl.set_xlim(72.5, 111.5)
             pl.set_xlabel('Frequency (GHz)')
             pl.set_ylabel('TA* (mK)')
-            pl.set_subplot_title("[%d:%d] -- %s Tint=%f hrs" %(min(obsList),max(obsList),hdu.header.SourceName, real_tint/4.0/3600.0))
+            pl.set_subplot_title("%s -- %s Tint=%f hrs" %(str(actual_chassis_list),hdu.header.SourceName, real_tint/4.0/3600.0))
         else:
             print 'using matplotlib_plot'
             import matplotlib.pyplot as pl
@@ -64,7 +85,7 @@ class RSRRunLineCheck():
             pl.xlim(72.5, 111.5)
             pl.xlabel('Frequency (GHz)')
             pl.ylabel('TA* (mK)')
-            pl.title("[%d:%d] -- %s Tint=%f hrs" %(min(obsList),max(obsList),hdu.header.SourceName, real_tint/4.0/3600.0))
+            pl.title("%s -- %s Tint=%f hrs" %(str(actual_chassis_list),hdu.header.SourceName, real_tint/4.0/3600.0))
         pl.savefig('rsr_summary.png', bbox_inches='tight')
 
 
