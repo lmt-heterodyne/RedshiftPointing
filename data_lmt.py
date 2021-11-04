5e4d42e0aea9c4ac9999d4ecb8b60fb6d1469270
Peter Teuben <teuben@gmail.com>
2021-11-04 10:39:47 -0400

    less verbose, but with optional debug

--- data_lmt.py
+++ data_lmt.py
@@ -1,9 +1,10 @@
 #
 # generic routine which finds the DATA_LMT root directory
+# backwards compatible with the old hardcoded /data_lmt system
 #
 import os
 
-def data_lmt(root = None):
+def data_lmt(root = None, debug=False):
     """
     Find and return the DATA_LMT root directory if no root is given:
       1. $DATA_LMT
@@ -12,12 +13,13 @@ def data_lmt(root = None):
     """
     if root == None:
         if 'DATA_LMT' in os.environ:
-           root = os.environ['DATA_LMT']
+            root = os.environ['DATA_LMT']
         elif os.path.exists('/data_lmt'):
-           root = '/data_lmt'
+            root = '/data_lmt'
         else:
-           print("FATAL: $DATA_LMT and /data_lmt do not exist")
-           print("       Most likely your code will now fail")
-    # debug
-    print("DATA_LMT = ",root)
+            # should never happen
+            print("FATAL: $DATA_LMT and /data_lmt do not exist")
+            print("       Most likely your code will now fail")
+    if debug:
+        print("DATA_LMT = ",root)
     return root
